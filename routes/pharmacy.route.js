fbc06d570ecdcbeec6db5e15f7939d485d35c917
Elanva <111335399+Elanva@users.noreply.github.com>
2023-01-11 15:38:11 +0530

    Add files via upload

--- routes/pharmacy.route.js
+++ routes/pharmacy.route.js
@@ -6,7 +6,7 @@ const connection = dbconfig.getConnection();
 //Medicine CRUD Operations
 
 //Pharrmacy CRUD Operations 
-//Get Pharrmacy Details by Patient Id
+//Get Pharmacy Details by Patient Id
 router.get('/patientlist/', (req, res, next) => {
       connection.query("select First_Name,Patient_Id,Doctor_Id,Phone_No, Status from Patients_Master where Status='Waiting For Pharmacy'",(err, results, fields) => {
         if (err) {
@@ -57,11 +57,11 @@ router.get('/getsinglePrescriptionById/:id', (req, res, next) => {
     });
 })
 
-// get medicine name by Id
-/*router.get('/:id', async (req, res, next) => {
-    const medicine_id = req.params.id;
-    let sql = "SELECT * FROM Medicine_Master where medicine_id = (?)";
-    connection.query(sql, [medicine_id], (err, results) => {
+// get Prescrription Detail by Patient Id
+router.get('/getprescriptionbyid/:id', async (req, res, next) => {
+    const patient_id = req.params.id;
+    let sql = "select Prescription_Details.Patient_Id,Prescription_Details.medicine_id,Prescription_Details.medicine_name,Prescription_Details.medicine_type,Prescription_Details.no_of_days,Prescription_Details.quantity,Prescription_Details.befre_or_aftr_food,Medicine_Master.single_qty_price,Medicine_Master.in_stock_total from Prescription_Details INNER JOIN Medicine_Master ON Prescription_Details.medicine_id=Medicine_Master.medicine_id where Prescription_Details.Patient_Id = (?)";
+    connection.query(sql, [patient_id], (err, results) => {
         if (err) {
             res.sendStatus(500);
             return;
@@ -70,10 +70,10 @@ router.get('/getsinglePrescriptionById/:id', (req, res, next) => {
             res.json({ "Data": "Data not Found", "Status": "false" });
         }
         else {
-            res.json({ "Data": results[0], "Status": "true" });
+            res.json({ "Data": results, "Status": "true" });
         }
     });
-});*/
+});
 
 //3.Add Doctors details
 router.post('/addprescriptionPrimary', async (req, res) => {
